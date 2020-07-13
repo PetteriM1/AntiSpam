43176c689087f4ddc80d0e96f95f8af822abfc3b
PetteriM1 <petteri.malkavaara1@gmail.com>
2020-07-13 18:04:02 +0300

    Update

--- src/main/java/suomicraftpe/core/antispam/ChatListener.java
+++ src/main/java/suomicraftpe/core/antispam/ChatListener.java
@@ -5,6 +5,11 @@ import cn.nukkit.event.EventHandler;
 import cn.nukkit.event.EventPriority;
 import cn.nukkit.event.Listener;
 import cn.nukkit.event.player.PlayerChatEvent;
+import cn.nukkit.event.player.PlayerQuitEvent;
+import org.apache.commons.lang3.StringUtils;
+
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * Ported from SuomiCraft PE Core
@@ -14,19 +19,18 @@ import cn.nukkit.event.player.PlayerChatEvent;
  */
 public class ChatListener implements Listener {
 
-    private String lastMsg;
-    private String lastSender;
-    private long lastTime;
+    private Map<String, String> lastMsg = new HashMap<>();
 
     @EventHandler(priority = EventPriority.LOWEST, ignoreCancelled = true)
     public void onChat(PlayerChatEvent e) {
         Player p = e.getPlayer();
         String name = p.getName();
+        long time = System.currentTimeMillis();
 
         if (ChatCooldownQueue.list.containsKey(name)) {
             p.sendMessage("§cYou are chatting too fast");
             e.setCancelled(true);
-            ChatCooldownQueue.list.put(name, System.currentTimeMillis());
+            ChatCooldownQueue.list.put(name, time);
             return;
         }
 
@@ -35,27 +39,35 @@ public class ChatListener implements Listener {
         if (message.length() > 150) {
             p.sendMessage("§cYour message is too long");
             e.setCancelled(true);
-            ChatCooldownQueue.list.put(name, System.currentTimeMillis());
+            ChatCooldownQueue.list.put(name, time);
             return;
         }
 
         if (message.startsWith("Horion - the best minecraft bedrock utility mod - ")) {
             e.setCancelled(true);
-            ChatCooldownQueue.list.put(name, System.currentTimeMillis());
+            ChatCooldownQueue.list.put(name, time);
             return;
         }
 
-        if (name.equals(lastSender) && message.equals(lastMsg) && System.currentTimeMillis() - lastTime < 10000) {
+        if (tooSimilar(name, message)) {
             p.sendMessage("§cDuplicated message");
             e.setCancelled(true);
-            ChatCooldownQueue.list.put(name, System.currentTimeMillis());
+            ChatCooldownQueue.list.put(name, time);
             return;
         }
 
-        lastMsg = message;
-        lastSender = name;
-        lastTime = System.currentTimeMillis();
+        lastMsg.put(name, message);
 
-        ChatCooldownQueue.list.put(name, lastTime);
+        ChatCooldownQueue.list.put(name, time);
+    }
+
+    @EventHandler
+    public void onQuit(PlayerQuitEvent e) {
+        lastMsg.remove(e.getPlayer().getName());
+    }
+
+    private boolean tooSimilar(String player, String message) {
+        String last = lastMsg.getOrDefault(player, "");
+        return message.equals(last) || StringUtils.getJaroWinklerDistance(message, last) > 0.95;
     }
 }
